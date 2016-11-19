73a2f0acfba0de86feaa4017bc0d32814c3924c9
Alen Hujdur <hujduralen@gmail.com>
2016-11-19 20:05:46 +0100

    ok

---
+++ Pretplatnici2_DAL/Service_DA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pretplatnici2_DAL
+{
+    public class Service_DA
+    {
+        public static void InsertPretplatnik(Pretplatnici p)
+        {
+            using (PretplatniciEntities dm = new PretplatniciEntities())
+            {
+                dm.usp_PretplatniciInsert(p.Ime, p.Prezime, p.Email, p.Lozinka, p.KorisnickoIme, p.Aktivan, p.StrucnaSpremaID, p.Slika, p.Cv);
+            }
+
+        }
+
+        public static List<StrucnaSprema> StrucneSpremeSelectAll()
+        {
+            using (PretplatniciEntities dm = new PretplatniciEntities())
+            {
+                return dm.usp_StrucneSpremeSelectAll().ToList();
+            }
+        }
+    }
+}
---
+++ Pretplatnici2_UI/DodajPretplatnika.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Pretplatnici2_DAL;
+
+namespace Pretplatnici2_UI
+{
+    public partial class DodajPretplatnika : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            NapuniSS();
+        }
+
+            private void NapuniSS()
+            {
+                List<StrucnaSprema> lista = Service_DA.StrucneSpremeSelectAll();
+                StrucnaSprema prazno = new StrucnaSprema();
+
+                prazno.StrucnaSpremaID = 0;
+                prazno.Skracenica = "-";
+                prazno.Naziv = "-";
+
+                lista.Insert(0, prazno);
+                ddlStrucneSpreme.DataSource = lista;
+                ddlStrucneSpreme.DataValueField = "StrucnaSpremaID";
+                ddlStrucneSpreme.DataTextField = "Skracenica";
+                ddlStrucneSpreme.DataBind();
+            }
+
+            protected void Sacuvaj_Click(object sender, EventArgs e)
+            {
+                Pretplatnici p = new Pretplatnici();
+                p.Ime = txtIme.Text;
+                p.Prezime = txtPrezime.Text;
+                p.KorisnickoIme = txtKorisnickoIme.Text;
+                p.Lozinka = txtLozinka.Text;
+                p.Email = txtEmail.Text;
+                p.StrucnaSpremaID = Convert.ToInt16(ddlStrucneSpreme.SelectedValue);
+
+                Pretplatnici2_DAL.Service_DA.InsertPretplatnik(p);
+        }
+        }
+    
+}
